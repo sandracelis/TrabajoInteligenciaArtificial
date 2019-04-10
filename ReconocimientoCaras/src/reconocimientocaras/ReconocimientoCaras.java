bb7aeeb591747484541df0450ec5e83da8eae69f
sandracelis <47510682+sandracelis@users.noreply.github.com>
2019-04-09 22:05:45 -0500

    carga del proyecto netbeans
    carga del proyecto netbeans con libreria open cv

---
+++ ReconocimientoCaras/src/reconocimientocaras/ReconocimientoCaras.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reconocimientocaras;
+
+import vista.FacialJFrame;
+
+/**
+ *
+ * @author CACERES
+ */
+public class ReconocimientoCaras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        FacialJFrame j = new FacialJFrame();
+        
+        j.iniciarFacialJFrame();
+    }
+    
+}
